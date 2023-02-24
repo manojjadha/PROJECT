e7ffbfe02d748b9a8f42a0a1bf84aea60921c045
THP <THP@THP_ManojJ>
2023-02-24 15:03:43 +0530

    First commit

---
+++ Automation/src/main/java/com/MyConnect/Automation/BrowserLoad.java
@@ -0,0 +1,71 @@
+package com.MyConnect.Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import okio.ByteString;
+
+
+
+public class BrowserLoad {
+	private static final String TimeUnit = null;
+	private static WebElement forgotpasswordLink;
+	WebDriver driver;
+
+	public static void main(String[] args) {
+	
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\THP\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+		
+			WebDriver driver = new ChromeDriver();
+			
+			driver.get("https://myconnectv1.tinggit.com/");
+			
+			
+			// Find the username and password in the login field them in
+			WebElement usernameField = driver.findElement(By.id("username"));
+			WebElement passwordField = driver.findElement(By.id("password"));
+			// Enter the login credential
+			usernameField.sendKeys("manoj.jadhav@thehigherpitch");
+			usernameField.sendKeys("Sara@9090");
+			//Find the click the login Button
+			RemoteWebElement loginButton = (RemoteWebElement) driver.findElement(By.id("login Button"));
+            loginButton.click();
+            //wait for next pageto load using implicit wait
+            driver.manage().timeouts().implicitlyWait(10,java.util.concurrent.TimeUnit.SECONDS);
+            //verify that login was successfull By checking for 
+            WebElement logoutButton = driver.findElement(By.id("logout"));
+            if(logoutButton.isDisplayed()) {
+            	
+            	System.out.println("login successfull");
+            }else {
+            	System.out.println("login faield");
+            	
+            	//click on forget password
+            	
+            	WebElement emailField = driver.findElement(By.linkText("Forget password"));
+            	forgotpasswordLink.click();
+            	//enter a registred email adress or username
+            	
+            	WebElement emailField1 = driver.findElement(By.id("email"));
+            	emailField.sendKeys("manoj.jadhav@thehigherpitch");
+            	// click on the submit Button
+            	WebElement submitButton = driver.findElement(By.xpath("//button[contains(text(), 'submit')]"));
+            	submitButton.click();
+            	//wait for password reset email to be sent
+            	//access the email acount and click on the password reset on the "submitButton"
+            	// verify that the password has been successfully change and the user is redirect to the login page 
+            	String expecturl =" https://myconnectv1.tinggit.com/";
+            	
+            	
+            	
+            }
+			
+			
+			
+	}
+	
+	
+}
